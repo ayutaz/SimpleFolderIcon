39c8042170c301fa3f141ad28ee0bd066d443b51
ayutaz <ka1357amnbpdr@gmail.com>
2022-02-22 22:57:16 +0900

    fix: check folder path for packages or assets/

--- Assets/SimpleFolderIcon/Editor/IconDictionaryCreator.cs
+++ Assets/SimpleFolderIcon/Editor/IconDictionaryCreator.cs
@@ -9,7 +9,8 @@ namespace SimpleFolderIcon.Editor
     public class IconDictionaryCreator : AssetPostprocessor
     {
         internal static Dictionary<string, Texture> IconDictionary;
-        private const string AssetFolderPath = "Packages/jp.ayutaz.simplefoldericon/Icons";
+        private const string PackageManagerIconPath = "Packages/jp.ayutaz.simplefoldericon/Icons";
+        private const string UnityPackageIconPath = "Assets/Simplefoldericon/Icons";
 
         private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
         {
@@ -26,7 +27,7 @@ namespace SimpleFolderIcon.Editor
 
         private static bool ContainsIconAsset(IEnumerable<string> assets)
         {
-            return assets.Any(str => ReplaceSeparatorChar(Path.GetDirectoryName(str)) == AssetFolderPath);
+            return assets.Any(str => (ReplaceSeparatorChar(Path.GetDirectoryName(str)) == PackageManagerIconPath) || (ReplaceSeparatorChar(Path.GetDirectoryName(str)) == UnityPackageIconPath));
         }
 
         private static string ReplaceSeparatorChar(string path)
@@ -37,16 +38,31 @@ namespace SimpleFolderIcon.Editor
         internal static void BuildDictionary()
         {
             var dictionary = new Dictionary<string, Texture>();
-
-            var dir = new DirectoryInfo(AssetFolderPath);
+            var iconFolderPath = GetIconPath();
+            var dir = new DirectoryInfo(iconFolderPath);
             var info = dir.GetFiles("*.png");
             foreach (var fileInfo in info)
             {
-                var texture = (Texture)AssetDatabase.LoadAssetAtPath(Path.Combine(AssetFolderPath, fileInfo.Name), typeof(Texture2D));
+                var texture = (Texture)AssetDatabase.LoadAssetAtPath(Path.Combine(iconFolderPath, fileInfo.Name), typeof(Texture2D));
                 dictionary.Add(Path.GetFileNameWithoutExtension(fileInfo.Name), texture);
             }
 
             IconDictionary = dictionary;
+        }
+
+        private static string GetIconPath()
+        {
+            if (Directory.Exists(PackageManagerIconPath))
+            {
+                return PackageManagerIconPath;
+            }
+
+            if (Directory.Exists(UnityPackageIconPath))
+            {
+                return UnityPackageIconPath;
+            }
+
+            return string.Empty;
         }
     }
 }
