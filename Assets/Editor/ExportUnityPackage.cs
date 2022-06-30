d45a128dbf78a123e9395651d9e690472de5055b
ayutaz <ka1357amnbpdr@gmail.com>
2022-06-30 22:18:51 +0900

    feat: UnityPackageを生成するMenu機能の作成

---
+++ Assets/Editor/ExportUnityPackage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+
+namespace Editor
+{
+    public static class ExportUnityPackage
+    {
+        [MenuItem("SimpleFolderIcon/Export Unity Package")]
+        private static void ExportPackage()
+        {
+            AssetDatabase.ExportPackage(
+                new string[]
+                {
+                    "Assets/SimpleFolderIcon",
+                },
+                "SimpleFolderIcon.unitypackage",
+                ExportPackageOptions.Recurse |
+                ExportPackageOptions.IncludeDependencies
+            );
+        }
+    }
+}
